0aa4037604d2ca70d9e210e009d6029c3d86dc03
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-03-18 13:15:24 +0100

    version 0.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='mailflash',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/nicolas-van/mailflash',
     license='BSD',
     author='Nicolas Vanhoren',
